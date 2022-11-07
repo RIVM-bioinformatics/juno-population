4849624aaedb64e0f30ee8bed78e58d5044dd999
Karim Hajji <karim.hajji@rivm.nl>
2022-11-07 10:40:34 +0100

    style: Run black

--- workflow/scripts/make_summary_csv.py
+++ workflow/scripts/make_summary_csv.py
@@ -6,11 +6,11 @@ import pandas as pd
 
 
 def combine_csv(root_dir=None):
-    csv_files = glob.glob(f'{root_dir}/*/*.csv')
+    csv_files = glob.glob(f"{root_dir}/*/*.csv")
     return pd.concat([pd.read_csv(f) for f in csv_files], ignore_index=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-i",
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         type=pathlib.Path,
         required=True,
         metavar="DIR",
-        help="Relative or absolute path to input directory from which all csv files should be merged."
+        help="Relative or absolute path to input directory from which all csv files should be merged.",
     )
     args = parser.parse_args()
     sys.stdout.write(combine_csv(args.input).to_csv())
