d7ef6ce0cdc96452d3c9e16c78e237a01394688f
Linda Visser <linda.visser@rivm.nl>
2022-11-16 11:53:06 +0100

    Fix: Update to latest GPSC database

--- database_locations.py
+++ database_locations.py
@@ -2,6 +2,6 @@ import pathlib
 
 species_database_locations = {
     "streptococcus_pneumoniae": pathlib.Path(
-        "/mnt/db/juno/poppunk/streptococcus/GPS_v4_references"
+        "/mnt/db/juno/poppunk/streptococcus/GPS_v6"
     ),
 }
